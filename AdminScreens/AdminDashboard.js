75071e4d03ce18796245ffe979a1e1890f908530
Brendan <bgmcmullen@comcast.net>
2024-07-01 18:30:30 -0400

    Administrator can now read and update events

--- AdminScreens/AdminDashboard.js
+++ AdminScreens/AdminDashboard.js
@@ -1,28 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, Button, StyleSheet, FlatList, Modal, TextInput, ImageBackground } from 'react-native';
-import { useTheme } from '../context/ThemeContext'; 
+import { useTheme } from '../context/ThemeContext';
+import axios from 'axios';
+import { CheckBox } from 'react-native-elements';
+import { ScrollView } from 'react-native-gesture-handler';
 
-const mockEvents = [
-  { id: '1', name: 'Food Drive', date: '2024-07-01', location: 'Community Center' },
-  { id: '2', name: 'Park Cleanup', date: '2024-07-15', location: 'City Park' },
-];
 
 const AdminDashboard = ({ navigation }) => {
   const { isDarkMode } = useTheme();
-  const [events, setEvents] = useState(mockEvents);
+  const [events, setEvents] = useState(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [newEventName, setNewEventName] = useState('');
   const [newEventDate, setNewEventDate] = useState('');
+  const [newEventContact, setNewEventContact] = useState('');
+  const [newEventDetails, setNewEventDetails] = useState('');
+  const [skills, setSkills] = useState({
+    Cleaning: false,
+    Organization: false,
+    "Heavy Lifting": false,
+    Spanish: false,
+    Cooking: false,
+    Gardening: false,
+    "Environmental Awareness": false
+  });
   const [newEventLocation, setNewEventLocation] = useState('');
+  // const [newEventLocation, setNewEventLocation] = useState('');
 
-  const handleCreateEvent = () => {
+  const API_URL = process.env.EXPO_PUBLIC_API_URL;
+  async function fetchEvents() {
+    // Fetch events from the database or API
+    
+
+    const fetchedEvents = await axios.get(`http://10.0.0.236:3000/api/v1/events`);
+    setEvents(fetchedEvents.data);
+  }
+
+  useEffect(() => {
+    fetchEvents()
+  }, []);
+
+  const handleCreateEvent = async () => {
+    const selectedSkills = Object.keys(skills).filter(skill => skills[skill]);
     const newEvent = {
-      id: (events.length + 1).toString(),
       name: newEventName,
       date: newEventDate,
       location: newEventLocation,
+      skills: selectedSkills,
+      detail: newEventDetails,
+      contact: newEventContact,
     };
-    setEvents([...events, newEvent]);
+    
+    const createdEvent = await axios.post(`http://10.0.0.236:3000/api/v1/events`, newEvent);
+    setEvents([...events, createdEvent.data]);
     setIsModalVisible(false);
   };
 
@@ -40,13 +69,23 @@ const AdminDashboard = ({ navigation }) => {
       <Text style={isDarkMode ? styles.textDark : styles.text}>{item.location}</Text>
       <Button
         title="Edit"
-        onPress={() => navigation.navigate('EditEvent', { eventId: item.id,
-        eventDetails: item,
-        updateEvent: handleUpdateEvent,
-         })}
+        onPress={() => navigation.navigate('EditEvent', {
+          eventId: item.id,
+          eventDetails: item,
+          updateEvent: handleUpdateEvent,
+        })}
       />
     </View>
   );
+
+  const toggleSkill = (skill) => {
+    setSkills(prevSkills => ({
+      ...prevSkills,
+      [skill]: !prevSkills[skill]
+    }));
+  };
+
+  const [height, setHeight] = useState(200);
 
   return (
     <ImageBackground source={require('../assets/background3.jpg')} style={styles.backgroundImage}>
@@ -70,27 +109,70 @@ const AdminDashboard = ({ navigation }) => {
         >
           <View style={styles.modalContainer}>
             <View style={styles.modalContent}>
-              <Text style={styles.modalTitle}>Create New Event</Text>
-              <TextInput
-                style={styles.input}
-                placeholder="Event Name"
-                value={newEventName}
-                onChangeText={text => setNewEventName(text)}
-              />
-              <TextInput
-                style={styles.input}
-                placeholder="Event Date"
-                value={newEventDate}
-                onChangeText={text => setNewEventDate(text)}
-              />
-              <TextInput
-                style={styles.input}
-                placeholder="Event Location"
-                value={newEventLocation}
-                onChangeText={text => setNewEventLocation(text)}
-              />
-              <Button title="Create" onPress={handleCreateEvent} />
-              <Button title="Cancel" onPress={() => setIsModalVisible(false)} />
+              <ScrollView>
+                <Text style={styles.modalTitle}>Create New Event</Text>
+                <Text style={styles.label}>Event Name</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Community Cleanup"
+                  placeholderTextColor='grey'
+                  value={newEventName}
+                  onChangeText={text => setNewEventName(text)}
+                />
+                <Text style={styles.label}>Event Date</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="10-10-2010"
+                  placeholderTextColor='grey'
+                  value={newEventDate}
+                  onChangeText={text => setNewEventDate(text)}
+                />
+                <Text style={styles.label}>Event Location</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Central Park"
+                  placeholderTextColor='grey'
+                  value={newEventLocation}
+                  onChangeText={text => setNewEventLocation(text)}
+                />
+                <Text style={styles.label}>Contact</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="email@example.com"
+                  placeholderTextColor='grey'
+                  value={newEventContact}
+                  onChangeText={text => setNewEventContact(text)}
+                />
+                <Text style={styles.label}>Details</Text>
+                <TextInput
+                  style={{borderWidth: 1, 
+                  borderRadius: 5,
+                  borderColor: '#ccc', 
+                  padding: 10,
+                  marginBottom: 10,
+                  height: Math.max(40, height+10) }}
+                  multiline
+                  placeholder="Join us for a community cleanup event at Central Park."
+                  placeholderTextColor='grey'
+                  value={newEventDetails}
+                  onChangeText={text => setNewEventDetails(text)}
+                  onContentSizeChange={(e) => {
+                    setHeight(e.nativeEvent.contentSize.height)
+                  }
+                }
+                />
+                <Text style={styles.label}>Required Skills</Text>
+                {Object.keys(skills).map(skill => (
+                  <CheckBox
+                    containerStyle={styles.checkBox}
+                    key={skill}
+                    title={skill}
+                    checked={skills[skill]}
+                    onPress={() => toggleSkill(skill)}
+                  />))}
+                <Button title="Create" onPress={handleCreateEvent} />
+                <Button title="Cancel" onPress={() => setIsModalVisible(false)} />
+              </ScrollView>
             </View>
           </View>
         </Modal>
@@ -184,6 +266,14 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 10,
     textAlign: 'center',
+  },
+  label: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginVertical: 2
+  },
+  checkBox: {
+    padding: 4
   },
   input: {
     borderWidth: 1,
